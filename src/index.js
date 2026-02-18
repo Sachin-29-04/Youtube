318af4ee041da4c8a557765d473d7cf0bea6f535
Sachin-29-04 <ss.sachinsahu.294@gmail.com>
2026-02-18 16:57:19 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -7,5 +7,18 @@
 
 import "dotenv/config";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running at port : ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed !!!", err);
+    process.exit(1);
+  });
+
